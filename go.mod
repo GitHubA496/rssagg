8e6b8b1c18f72bf8d7fc0d6742370adce522eb10
GITHUBLEARNER <darkest715@gmail.com>
2025-03-01 14:21:02 +0530

    boilerplate for http server complete

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/GitHubA496/rssagg
 
 go 1.23.6
+
+require github.com/joho/godotenv v1.5.1
+
+require (
+	github.com/go-chi/chi v1.5.5 
+	github.com/go-chi/cors v1.2.1 
+)
--- main.go
+++ main.go
@@ -1,9 +1,49 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
 func main() {
-	println("Hello, World!")
-	println("Hello, World!")
-	println("Hello, World!")
-	println("Hello, World!")
-	println("Hello, World!")
+
+	godotenv.Load(".env")
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT is not found wihtin the env")
+	}
+	fmt.Print(port)
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://*", "https://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+	v1router := chi.NewRouter()
+	v1router.Get("/healthz", handlerReadiness)
+	v1router.Get("/err", handlerError)
+
+	router.Mount("/v1", v1router)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + port,
+	}
+	log.Printf("Server starting on the port %v", port)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
